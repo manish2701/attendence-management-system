627929064c8a96da3430073b1af6d0f831bf1726
manish2701 <manish2701j@gmail.com>
2023-04-07 16:30:33 +0530

    main file

--- attendence management system.py
+++ attendence management system.py
@@ -1,6 +1,21 @@
 import mysql.connector
 
-con = mysql.connector.connect(user='root', password='Manish@2701',host='localhost',database='classes')
+con = mysql.connector.connect(user='root', password='Manish@2701', host='localhost', database='classes')
         
 cursor = con.cursor()
+
+class_no = input("enter class number : ")
+
+
+
+cursor.execute(f"select * from class{class_no}")
+
+rows=cursor.fetchall()
+
+
+for row in rows:
+    for col in row:
+        print(col,end=' ')
+    print()
+    
 
