106ad5adcc75d8cd650f97ecef3a1945a7997ffa
manish2701 <manish2701j@gmail.com>
2023-04-07 22:49:07 +0530

    main file

--- attendence management system.py
+++ attendence management system.py
@@ -7,15 +7,27 @@ cursor = con.cursor()
 class_no = input("enter class number : ")
 
 
-
-cursor.execute(f"select * from class{class_no}")
-
-rows=cursor.fetchall()
-
-
-for row in rows:
-    for col in row:
-        print(col,end=' ')
+def show_name():
     print()
+    cursor.execute(f"select * from class{class_no}")
+    rows=cursor.fetchall()
+
+    cursor.execute(f"desc class{class_no}")
+    desc=cursor.fetchall()
+    
+    print("your list of students is below :\n")
+
+    print("Roll No |          Name         ")
+
+    for row in rows:
+        for col in row:
+            if col == row[-1]:
+                break
+            if col == row[1]:
+                print(f'    {col.title()}')
+            else:
+                print(' '*2,col,end='\t|')
     
 
+#use this in a systematic function when you return
+show_name()
